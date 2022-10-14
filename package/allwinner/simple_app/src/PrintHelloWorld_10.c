a4ca78bc048e6f1a160e8f673713a0eeb23cd2a5
duhylee <duhylee@gmail.com>
2022-10-14 14:19:03 +0900

    modified simple app

--- package/allwinner/simple_app/src/PrintHelloWorld_10.c
+++ package/allwinner/simple_app/src/PrintHelloWorld_10.c
@@ -1,12 +1,30 @@
 //PrintHelloWorld_10.c
 #include <stdio.h>
+#include <signal.h>
+#include <unistd.h>
+#include <string.h>
+#include <errno.h>
+#include <stdlib.h>
+
+void sys_mdelay(unsigned int ms_delay)
+{
+    usleep(ms_delay*1000);
+}
+
 void PrintHelloWorld_10()
 {
-	int i = 0;
-	for(i=0; i<10; i++)
-	{
-		printf("Hello World\n");
-	}
+
+    //int i = 0;
+    int received = 0;
+
+    printf("============== Simple Application Test ==============\n");
+
+    //for(i=0; i<10; i++)
+    while(!received)
+    {
+      printf("Hello World\n");
+      sys_mdelay(1000);
+    }
 }
 
 
